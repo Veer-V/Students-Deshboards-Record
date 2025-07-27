8878ae600879280400b62388598194c3c5c9f49d
Veer-V <princeofworld453@gmail.com>
2025-07-27 21:03:40 +0530

    cat

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ import streamlit as st
 import pandas as pd
 import joblib
 
+def generate_tip(student_data):
+    if student_data["Attendance_Percentage"] < 75:
+        return "Improve attendance to at least 75%."
+    elif student_data["Internal_Assessment_1"] < 50 or student_data["Internal_Assessment_2"] < 50:
+        return "Focus on improving internal assessment scores."
+    elif student_data["Participation_Score"] < 3:
+        return "Increase participation in class activities."
+    else:
+        return "Keep up the good work!"
+    
 # Load model and data
 model = joblib.load("model_pipeline.pkl")  # save model earlier using joblib
 df = pd.read_csv("student_performance_60.csv")
@@ -28,15 +38,7 @@ st.subheader(f"Result: {result}")
 st.write(f"📊 Recommendation: {generate_tip(student_data)}")
 
 st.write("📋 Student Details", student_data)
-def generate_tip(student_data):
-    if student_data["Attendance_Percentage"] < 75:
-        return "Improve attendance to at least 75%."
-    elif student_data["Internal_Assessment_1"] < 50 or student_data["Internal_Assessment_2"] < 50:
-        return "Focus on improving internal assessment scores."
-    elif student_data["Participation_Score"] < 3:
-        return "Increase participation in class activities."
-    else:
-        return "Keep up the good work!"
+
 uploaded_file = st.file_uploader("Upload updated student data CSV")
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
